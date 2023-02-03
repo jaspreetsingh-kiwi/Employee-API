be0e51424a338e654ac4d19f0846ab5c21d4f4b1
jaspreetsingh-kiwi <118871632+jaspreetsingh-kiwi@users.noreply.github.com>
2023-02-03 19:38:20 +0530

    update

--- Employee_Details/EmployeeDetails/settings.py
+++ Employee_Details/EmployeeDetails/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'EmployeeDetails.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # add this
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,13 +80,12 @@ WSGI_APPLICATION = 'EmployeeDetails.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Employee',
-        'USER': 'postgres',
-        'PASSWORD': 123456,
-        'HOST': 'localhost',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASS'),
+        'HOST': os.getenv('DATABASE_HOST'),
     }
 }
-
 
 
 # Password validation
